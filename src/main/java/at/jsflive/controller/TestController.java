853d39252648c7e6a57f7c308199051207ae57d6
Michael Kurz <michael.kurz@irian.at>
2012-09-05 20:25:45 +0200

    initial version

---
+++ src/main/java/at/jsflive/controller/TestController.java
@@ -0,0 +1,35 @@
+package at.jsflive.controller;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@ManagedBean
+public class TestController {
+
+    private Long testLong;
+
+    public Validator getMyValidator() {
+        return new MyValidator();
+    }
+
+    public Long getTestLong() {
+        return testLong;
+    }
+
+    public void setTestLong(Long testLong) {
+        this.testLong = testLong;
+    }
+
+    public static class MyValidator implements Validator {
+        public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+            if (((Long)value) < 10) {
+                throw new ValidatorException(new FacesMessage("error"));
+            }
+        }
+    };
+
+}
