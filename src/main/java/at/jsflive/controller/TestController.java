5b189fe8f19fa663459f11cd2abd7f023f609f38
Michael Kurz <michael.kurz@irian.at>
2012-09-06 14:48:32 +0200

    minor refactoring

--- src/main/java/at/jsflive/controller/TestController.java
+++ src/main/java/at/jsflive/controller/TestController.java
@@ -10,21 +10,21 @@ import javax.faces.validator.ValidatorException;
 @ManagedBean
 public class TestController {
 
-    private Long testLong;
+    private Long longValue;
 
-    public Validator getMyValidator() {
-        return new MyValidator();
+    public Validator getLongValidator() {
+        return new LongValidator();
     }
 
-    public Long getTestLong() {
-        return testLong;
+    public Long getLongValue() {
+        return longValue;
     }
 
-    public void setTestLong(Long testLong) {
-        this.testLong = testLong;
+    public void setLongValue(Long longValue) {
+        this.longValue = longValue;
     }
 
-    public static class MyValidator implements Validator {
+    public static class LongValidator implements Validator {
         public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
             if (((Long)value) < 10) {
                 throw new ValidatorException(new FacesMessage("error"));
